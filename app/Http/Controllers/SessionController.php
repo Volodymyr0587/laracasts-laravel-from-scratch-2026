f2bae5d939fa3265a5b16aea130c702028671c6d
Volodymyr Honcharov <ladimirvm87@gmail.com>
2026-03-25 17:59:53 +0200

    add Request to destroy method and invalidate, and regenerate token

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -35,9 +35,12 @@ class SessionController extends Controller
 
     }
 
-    public function destroy()
+    public function destroy(Request $request)
     {
         Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
         return redirect('/');
     }
