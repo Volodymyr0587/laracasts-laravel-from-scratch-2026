5aebf0b2b2c214448d104517f571ba36daf240fb
Volodymyr Honcharov <ladimirvm87@gmail.com>
2026-03-28 13:29:23 +0200

    Actionable Steps

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -38,6 +38,30 @@
                 </div>
             </x-card>
 
+            @if ($idea->steps->count())
+                <div>
+                    <h3 class="font-bold text-xl mt-6 mb-1">Actionable Steps</h3>
+
+                    <div class="mt-3 space-y-2">
+                        @foreach ($idea->steps as $step)
+                            <x-card>
+                                <form method="POST" action="{{ route('step.update', $step) }}">
+                                    @csrf
+                                    @method('PATCH')
+
+                                    <div class="flex items-center gap-x-3">
+                                        <button type="submit" role="checkbox"
+                                            class="size-5 flex items-center justify-center rounded-lg text-primary-foreground {{ $step->completed ? 'bg-primary' : 'border border-primary' }}">&check;</button>
+                                        <span
+                                            class="{{ $step->completed ? 'line-through text-muted-foreground' : '' }}">{{ $step->description }}</span>
+                                    </div>
+                                </form>
+                            </x-card>
+                        @endforeach
+                    </div>
+                </div>
+            @endif
+
             @if ($idea->links->count())
                 <div>
                     <h3 class="font-bold text-xl mt-6 mb-1">Links</h3>
